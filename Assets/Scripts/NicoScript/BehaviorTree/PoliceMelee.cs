e0b4265a9f4a922f0b075f81c0e4ee63a2deb6e1
KhaZixNb1Fan <n_niruhtam1@hotmail.com>
2025-12-04 19:21:09 -0500

    Patrol

--- Assets/Scripts/NicoScript/BehaviorTree/PoliceMelee.cs
+++ Assets/Scripts/NicoScript/BehaviorTree/PoliceMelee.cs
@@ -4,17 +4,24 @@ using UnityEngine.AI;
 public class PoliceMelee : BehaviorTree
 {
     [SerializeField] Transform[] targets;
-    Interrupt interrupt;
+
+    Interrupt Interrupt;
+
+    GameObject player;
+
+    Transform playerTransform;
     protected override void InitializeTree()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
         //************************************* Conditions *************************************//
-
+        Conditions meleeCondition = new WithinRange(agent.transform, player, 30f);
+        
 
         //************************************* Interrupt *************************************//
-        interrupt = new Interrupt(null, this);
+        Interrupt = new Interrupt(new Conditions[] { meleeCondition, }, this);
+        
 
 
         //************************************* Nodes *************************************//
@@ -22,25 +29,39 @@ public class PoliceMelee : BehaviorTree
         GoToTarget goTo2 = new GoToTarget(agent, targets[1], 4f, null, this);
         GoToTarget goTo3 = new GoToTarget(agent, targets[2], 4f, null, this);
         GoToTarget goTo4 = new GoToTarget(agent, targets[3], 4f, null, this);
+        GoToTarget chasePlayer = new GoToTarget(agent, player.transform, 2f, null, this);
         Wait wait2 = new Wait(2, null, this);
-        Wait wait4 = new Wait(4, null, this);
+        Wait wait4 = new Wait(2, null, this);
 
 
         //*************************************** Sequences *************************************//
         Sequence patrolSequence = new Sequence(new Node[] { goTo1, wait2, goTo2, wait2, goTo3, wait2, goTo4, wait2 }, null , this);
-
+        Sequence meleeSequence = new Sequence(new Node[] { chasePlayer }, null, this);
         //*************************************** Root Node *************************************//
         root = new Selector(new Node[] { patrolSequence }, null, this);
     }
 
     private void OnDisable()
     {
-        interrupt.Stop();
+        if(Interrupt != null)
+            Interrupt.Stop();
     }
 
     private void OnEnable()
     {
-        if (interrupt != null)
-            interrupt.Start();
+        if(Interrupt != null)
+            Interrupt.Start();
+    }
+    private void OnDrawGizmos()
+    {
+
+        Vector3 pos = transform.position;
+
+        // Melee zone
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pos, 20f);
+
+        if (player != null)
+            Gizmos.DrawLine(pos, player.transform.position);
     }
 }
