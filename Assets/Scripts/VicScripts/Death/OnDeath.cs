c0c93f5dc2f2e82aa3e017a4c91530c98a2b487b
DidlydooForYou <victorcotenicholson@gmail.com>
2025-12-14 00:07:31 -0500

    Wobble cam et merge fix

---
+++ Assets/Scripts/VicScripts/Death/OnDeath.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class OnDeath : MonoBehaviour
+{
+    [SerializeField] float fadeDuration;
+    [SerializeField] GameObject wasted;
+    [SerializeField] float wobbleSpeed = 2f;
+    [SerializeField] float wobbleAmount = 5f;
+
+
+    private Camera cameraDeath;
+    private Camera cameraMain;
+    private Volume volume;
+
+    private Quaternion originalRot;
+
+    bool isDead = false;
+    void Awake()
+    {
+        cameraDeath = GameObject.FindGameObjectWithTag("DeathCam").GetComponent<Camera>();
+        cameraMain = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        volume = cameraDeath.gameObject.GetComponent<Volume>();
+        originalRot = cameraDeath.transform.rotation;
+        cameraDeath.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        clickDeath();
+        if (isDead)
+            Wobble();
+    }
+
+    void clickDeath()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            cameraMain.gameObject.SetActive(false);
+            cameraDeath.gameObject.SetActive(true);
+            StartCoroutine(DeathIn());
+            StartCoroutine(WastedImage());
+            StartCoroutine(SlowMo());
+            isDead = true;
+        }
+    }
+
+    void Wobble()
+    {
+        float t = Time.unscaledTime;
+
+        float x = (Mathf.PerlinNoise(t * wobbleSpeed, 0.0f) - 0.5f) * 2f * wobbleAmount;
+
+        Quaternion targetRot = originalRot * Quaternion.Euler(x, 0f, 15f);
+
+        cameraDeath.transform.rotation = Quaternion.Lerp(cameraDeath.transform.rotation, targetRot, Time.deltaTime * 2f);
+    }
+
+    IEnumerator DeathIn()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            volume.weight = Mathf.Lerp(0f, 1f, t / fadeDuration);
+            yield return null;
+        }
+
+        volume.weight = 1f;
+    }
+
+    IEnumerator WastedImage()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        wasted.gameObject.SetActive(true);
+    }
+
+    IEnumerator SlowMo()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.unscaledDeltaTime;
+            Time.timeScale = Mathf.Lerp(1f, 0.1f, t / fadeDuration);
+            yield return null;
+        }
+    }
+}
